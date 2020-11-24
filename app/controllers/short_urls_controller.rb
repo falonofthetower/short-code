db49d08ea0a0d51e1ce7f6d0ec1ac1c42c763dec
Peter Karth <falonofthetower@gmail.com>
2020-11-24 16:55:34 -0500

    Add the creation of ShortUrl
    Broke off the display of the messages into a helper.

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -11,9 +11,22 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    @short_url = ShortUrl.new(short_url_params)
+
+    if @short_url.save
+      render status: 200, json: { short_code: @short_url.short_code }
+    else
+      render status: 422, json: { errors: helpers.humanize_errors(@short_url) }
+    end
   end
 
   def show
+  end
+
+  private
+
+  def short_url_params
+    params.permit(:full_url)
   end
 
 end
--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -1,2 +1,8 @@
 module ShortUrlsHelper
+  def humanize_errors(ar_object)
+    ar_object.errors.messages.map do |k, messages|
+      messages.map do |message| "#{k} #{message}".humanize.capitalize 
+      end
+    end.join(',')
+  end
 end
