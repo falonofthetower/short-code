3d8264bdd8eeb7351177ce3d9ccf9b2c8af8b4ee
Peter Karth <falonofthetower@gmail.com>
2020-11-24 16:22:28 -0500

    Add algorithm for short code
    I did a little digging on this one and realized pretty quickly there was
    only one way to solve this given the way the specs are setup. I won't
    claim much originality here, but it doesn't appear that it was intended
    to be original.

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,10 +2,13 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
   PUBLIC_ATTRIBUTES = [:full_url, :title]
+  BASE = CHARACTERS.size
 
   validates :full_url, valid_url: true
 
   def short_code
+    return nil unless full_url
+    encode(id)
   end
 
   def public_attributes
@@ -15,6 +18,19 @@ class ShortUrl < ApplicationRecord
   end
 
   def update_title!
+  end
+
+  private
+
+    def encode(id)
+    i = id
+
+    String.new('').tap do |code|
+      while i.positive?
+        code << CHARACTERS[i % BASE]
+        i /= BASE
+      end
+    end.reverse
   end
 
 end
